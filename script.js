fa6cf1981e43be7205a5b1a8098b8a0624e73192
AlrSystems <alfredbayon@hotmail.com>
2025-02-02 19:38:55 +0100

    iniciacion

---
+++ script.js
@@ -0,0 +1,9 @@
+// Archivo: script.js
+
+// Función para mostrar un mensaje de saludo
+function mostrarSaludo() {
+    alert("¡Hola! Bienvenido a mi página web.");
+}
+
+// Llamar a la función cuando se carga la página
+window.onload = mostrarSaludo;
